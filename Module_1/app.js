53f15774e08c053b61a87eeb1c10d4fa317798b1
Jayshii <jpdatani@gmail.com>
2017-07-17 01:43:21 +0530

    Update app.js

--- Module_1/app.js
+++ Module_1/app.js
@@ -1,2 +1,25 @@
 (function(){
-'use strict'
+'use strict';
+    angular.module('LunchCheck',[])
+    .controller('LunchCheckController',LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.message='';
+  $scope.checked=false;
+  $scope.items='';
+
+  $scope.checkitems = function() {
+    var array_items = $scope.items.split(',');
+    var array_items_withoutspace = array_items.filter(function(z) {
+      return z.trim() !== '';
+    });
+    if(array_items_withoutspace.length <= 3) {
+      $scope.message = 'Enjoy!';
+    }
+    else {
+      $scope.message= 'Too much!';
+    }
+  }
+}
+})();
