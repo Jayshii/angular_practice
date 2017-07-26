837e722d9451a445c86820c83dda56a8cdbf4cfb
Jayshii <jpdatani@gmail.com>
2017-07-26 12:15:38 +0530

    Update app.js

--- Module_2/app.js
+++ Module_2/app.js
@@ -1,7 +1,38 @@
 (function(){
 'use strict'
- var shoppinglistmodule = angular.module('ShoppingListCheck',[])
- shoppinglistmodule.controller('ToBuyController',ToBuyController)
- shoppinglistmodule.controller('alreadyboughtcontroller',alreadyboughtcontroller)
-  shoppinglistmodule.service('shoppio')
+ var shoppingListModule = angular.module('ShoppingListCheck',[])
+ shoppingListModule.controller('toBuyController',toBuyController)
+ shoppingListModule.controller('alreadyBoughtController',alreadyBoughtController)
+  shoppingListModule.service('shoppingService',shoppingService)
+ 
+ alreadyBoughtController.$inject=['shoppingService']
+ toBuyController.$inject=['shoppingService']
+ 
+ function shoppingService(){
+ var service = this;
+  var toBuyList=
+      [
+       {name:'cookies',quantity:10},
+       {name:'chocolates',quantity:15},
+       {name:'butter',quantity:20},
+       {name:'bread',quantity:25},
+       {name:'peanut',quantity:5}  
+      ]
+  var boughtList=[];
+  service.getBoughtItems = function()
+  {
+   return boughtList;
+  }
+  
+  service.getToBuyItems = function()
+  {
+   return toBuyList;
+  }
+  service.buyItem=function(index)
+  {
+   var itemIndex = toBuyList[index];
+   boughtList.push(itemIndex)
+   toBuyList.splice(itemIndex,1)
+  }
+ }
 })();
