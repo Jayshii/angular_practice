5f28e6a9e483d98ca46a4203fadcea7b785da2d7
Jayshii <jpdatani@gmail.com>
2017-07-27 13:04:21 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,54 @@
+(function(){
+'use strict'
+ var shoppingListModule = angular.module('ShoppingListCheck',[])
+ shoppingListModule.controller('toBuyController',toBuyController)
+ shoppingListModule.controller('alreadyBoughtController',alreadyBoughtController)
+  shoppingListModule.service('shoppingService',shoppingService)
+ 
+ alreadyBoughtController.$inject=['shoppingService']
+ toBuyController.$inject=['shoppingService']
+ 
+ function shoppingService(){
+ var service = this;
+  var toBuyList=
+      [
+       {name:'cookies',quantity:10},
+       {name:'chocolates',quantity:15},
+       {name:'butter',quantity:20},
+       {name:'bread',quantity:25},
+       {name:'peanut',quantity:5}  
+      ]
+  var boughtList=[];
+  service.getBoughtItems = function()
+  {
+   return boughtList;
+  }
+  
+  service.getToBuyItems = function()
+  {
+   return toBuyList;
+  }
+  service.buyItem=function(index)
+  {
+   var itemIndex = toBuyList[index];
+   boughtList.push(itemIndex)
+   toBuyList.splice(itemIndex,1)
+  }
+ }
+ 
+ function alreadyBoughtController(shoppingService)
+ {
+  var bought = this;
+  bought.items = shoppingService.getBoughtItems();
+ }
+ 
+ 
+ function toBuyController(shoppingService)
+ {
+  var tobuy = this;
+  tobuy.buyItem = function(index){
+   shoppingService.buyItem(index);
+  }
+  tobuy.items = shoppingService.getToBuyItems();
+ }
+})();
